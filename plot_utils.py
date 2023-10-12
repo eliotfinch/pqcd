bbfe9e114f608e246772c78e7c9b2ff6fa08576d
Eliot Finch <eliotfinch@gmail.com>
2023-10-12 11:53:28 -0700

    Update plots

---
+++ plot_utils.py
@@ -0,0 +1,11 @@
+rcparams = {
+    'font.size': 14,
+    'axes.titlesize': 14,
+    'axes.labelsize': 14,
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'legend.fontsize': 12,
+    'font.family': 'serif',
+    'font.sans-serif': ['Computer Modern Roman'],
+    'text.usetex': True,
+    }
