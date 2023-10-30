4d42218e8aeadf21f1950638db738f75ab9d3067
Eliot Finch <eliotfinch@gmail.com>
2023-10-30 11:18:18 -0700

    Combine util files

--- utils.py
+++ utils.py
@@ -2,6 +2,18 @@ import numpy as np
 
 from scipy.constants import c, e, m_u
 
+rcparams = {
+    'font.size': 14,
+    'axes.titlesize': 14,
+    'axes.labelsize': 14,
+    'xtick.labelsize': 12,
+    'ytick.labelsize': 12,
+    'legend.fontsize': 12,
+    'font.family': 'serif',
+    'font.sans-serif': ['Computer Modern Roman'],
+    'text.usetex': True,
+    }
+
 def to_GeV_per_cubic_femtometre(x):
 
     # Convert to J/cm^3
