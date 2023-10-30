f6cd332bda75763d672b38a83a4c73bed65fe5d6
Eliot Finch <eliotfinch@gmail.com>
2023-10-30 11:18:41 -0700

    Use more samples in temperance

--- temperance_quantiles.py
+++ temperance_quantiles.py
@@ -1,18 +1,19 @@
 import temperance.core.result as result
-from temperance.core.result import EoSPosterior
+import temperance.plotting.get_quantiles as get_quantiles
 
+from temperance.core.result import EoSPosterior
 from temperance.sampling.eos_prior import EoSPriorSet
 
-import temperance.plotting.get_quantiles as get_quantiles
-if  __name__ == "__main__":
-    eos_posterior = EoSPosterior.from_csv("collated_np_all_post.csv", label="astro")
-    #prior_quantiles=  tmplot.get_quantiles.get_quantiles(eos_posterior, weight_columns=[])
-    astro_weight_columns = [result.WeightColumn(
-        name="logweight_total", is_log=True,
-        is_inverted=False)]
-    posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
-        eos_posterior,
-        weight_columns=astro_weight_columns, verbose=True, save_path='quantiles.csv')
-    print(eos_posterior.weight_columns_available)
-    print(eos_posterior.samples)
-    default_eos_prior = EoSPriorSet.get_default()
+eos_posterior = EoSPosterior.from_csv("collated_np_all_post.csv", label="astro")
+
+astro_weight_columns = [
+    result.WeightColumn(name="logweight_total", is_log=True, is_inverted=False)
+    ]
+
+posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
+    eos_posterior, 
+    weight_columns=astro_weight_columns, 
+    verbose=True, 
+    max_num_samples=80000,
+    save_path='quantiles/p_of_eps_quantiles.csv'
+    )
