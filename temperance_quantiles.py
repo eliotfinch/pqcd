3f9736f1463a47c30e29e54dae366ca72b1316d7
Eliot Finch <eliotfinch@gmail.com>
2023-10-30 13:53:12 -0700

    Prepare ns10 quantile calculation

--- temperance_quantiles.py
+++ temperance_quantiles.py
@@ -4,12 +4,15 @@ import temperance.core.result as result
 import temperance.plotting.get_quantiles as get_quantiles
 
 from temperance.core.result import EoSPosterior
-from temperance.sampling.eos_prior import EoSPriorSet
 
-eos_posterior = EoSPosterior.from_csv("collated_np_all_post.csv", label="astro")
+# eos_posterior = EoSPosterior.from_csv("collated_np_all_post.csv", label="astro")
+eos_posterior = EoSPosterior.from_csv('collated_eos.csv', label='astro')
 
+# astro_weight_columns = [
+#     result.WeightColumn(name='logweight_total', is_log=True, is_inverted=False)
+#     ]
 astro_weight_columns = [
-    result.WeightColumn(name="logweight_total", is_log=True, is_inverted=False)
+    result.WeightColumn(name='weight_ns10_Xmarg', is_log=False, is_inverted=False)
     ]
 
 posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
@@ -17,5 +20,5 @@ posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
     weight_columns=astro_weight_columns, 
     verbose=True, 
     max_num_samples=80000,
-    save_path='quantiles/p_of_eps_quantiles.csv'
+    save_path='quantiles/p_of_eps_quantiles_ns10_Xmarg.csv'
     )
