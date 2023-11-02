9a32a44e4774d2de04b6ffb144d43c1476bb6873
Eliot Finch <eliotfinch@gmail.com>
2023-11-02 11:50:59 -0700

    Prepare fixed X quantile calculation

--- temperance_qcd_quantiles.py
+++ temperance_qcd_quantiles.py
@@ -8,57 +8,90 @@ from temperance.core.result import EoSPosterior
 # eos_posterior = EoSPosterior.from_csv("collated_np_all_post.csv", label="astro")
 eos_posterior = EoSPosterior.from_csv('collated_eos.csv', label='astro')
 
-# astro_weight_columns = [
-#     result.WeightColumn(name='logweight_total', is_log=True, is_inverted=False)
-#     ]
-
 for ns in [5, 6, 7, 8, 9, 10]:
 
-    astro_weight_columns = [
-        result.WeightColumn(name=f'weight_ns{ns:02}_Xmarg', is_log=False, is_inverted=False)
-        ]
+    # Marginalised over X
 
-    # Pressure vs energy density
-    posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
-        eos_posterior, 
-        weight_columns=astro_weight_columns, 
-        verbose=True, 
-        max_num_samples=80000,
-        save_path=f'quantiles/p_of_eps_quantiles_ns{ns:02}_Xmarg.csv'
-        )
+    # astro_weight_columns = [
+    #     result.WeightColumn(name=f'weight_ns{ns:02}_Xmarg', is_log=False, is_inverted=False)
+    #     ]
 
-    # Pressure vs baryon density
-    posterior_quantiles = get_quantiles.get_p_of_rho_quantiles(
-        eos_posterior, 
-        weight_columns=astro_weight_columns, 
-        verbose=True, 
-        max_num_samples=80000,
-        save_path=f'quantiles/p_of_rho_quantiles_ns{ns:02}_Xmarg.csv'
-        )
+    # # Pressure vs energy density
+    # posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
+    #     eos_posterior, 
+    #     weight_columns=astro_weight_columns, 
+    #     verbose=True, 
+    #     max_num_samples=80000,
+    #     save_path=f'quantiles/p_of_eps_quantiles_ns{ns:02}_Xmarg.csv'
+    #     )
 
-    # Speed of sound vs baryon density
-    posterior_quantiles = get_quantiles.get_cs2_of_rho_quantiles(
-        eos_posterior, 
-        weight_columns=astro_weight_columns, 
-        verbose=True, 
-        max_num_samples=80000,
-        save_path=f'quantiles/cs2_of_rho_quantiles_ns{ns:02}_Xmarg.csv'
-        )
+    # # Pressure vs baryon density
+    # posterior_quantiles = get_quantiles.get_p_of_rho_quantiles(
+    #     eos_posterior, 
+    #     weight_columns=astro_weight_columns, 
+    #     verbose=True, 
+    #     max_num_samples=80000,
+    #     save_path=f'quantiles/p_of_rho_quantiles_ns{ns:02}_Xmarg.csv'
+    #     )
 
-    # Mass vs radius
-    posterior_quantiles = get_quantiles.get_r_of_m_quantiles(
-        eos_posterior, 
-        weight_columns=astro_weight_columns, 
-        verbose=True, 
-        max_num_samples=80000,
-        save_path=f'quantiles/r_of_m_quantiles_ns{ns:02}_Xmarg.csv'
-        )
+    # # Speed of sound vs baryon density
+    # posterior_quantiles = get_quantiles.get_cs2_of_rho_quantiles(
+    #     eos_posterior, 
+    #     weight_columns=astro_weight_columns, 
+    #     verbose=True, 
+    #     max_num_samples=80000,
+    #     save_path=f'quantiles/cs2_of_rho_quantiles_ns{ns:02}_Xmarg.csv'
+    #     )
 
-    # Lambda vs mass
-    posterior_quantiles = get_quantiles.get_lambda_of_m_quantiles(
-        eos_posterior, 
-        weight_columns=astro_weight_columns, 
-        verbose=True, 
-        max_num_samples=80000,
-        save_path=f'quantiles/lambda_of_m_quantiles_ns{ns:02}_Xmarg.csv'
-        )
+    # # Mass vs radius
+    # posterior_quantiles = get_quantiles.get_r_of_m_quantiles(
+    #     eos_posterior, 
+    #     weight_columns=astro_weight_columns, 
+    #     verbose=True, 
+    #     max_num_samples=80000,
+    #     save_path=f'quantiles/r_of_m_quantiles_ns{ns:02}_Xmarg.csv'
+    #     )
+
+    # # Lambda vs mass
+    # posterior_quantiles = get_quantiles.get_lambda_of_m_quantiles(
+    #     eos_posterior, 
+    #     weight_columns=astro_weight_columns, 
+    #     verbose=True, 
+    #     max_num_samples=80000,
+    #     save_path=f'quantiles/lambda_of_m_quantiles_ns{ns:02}_Xmarg.csv'
+    #     )
+
+    # Fixed X
+
+    for X in [0.5, 2]:
+
+        astro_weight_columns = [
+            result.WeightColumn(name=f'weight_ns{ns:02}_X{X}', is_log=False, is_inverted=False)
+            ]
+
+        # Pressure vs energy density
+        posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
+            eos_posterior, 
+            weight_columns=astro_weight_columns, 
+            verbose=True, 
+            max_num_samples=80000,
+            save_path=f'quantiles/p_of_eps_quantiles_ns{ns:02}_X{X}.csv'
+            )
+
+        # Pressure vs baryon density
+        posterior_quantiles = get_quantiles.get_p_of_rho_quantiles(
+            eos_posterior, 
+            weight_columns=astro_weight_columns, 
+            verbose=True, 
+            max_num_samples=80000,
+            save_path=f'quantiles/p_of_rho_quantiles_ns{ns:02}_X{X}.csv'
+            )
+
+        # Speed of sound vs baryon density
+        posterior_quantiles = get_quantiles.get_cs2_of_rho_quantiles(
+            eos_posterior, 
+            weight_columns=astro_weight_columns, 
+            verbose=True, 
+            max_num_samples=80000,
+            save_path=f'quantiles/cs2_of_rho_quantiles_ns{ns:02}_X{X}.csv'
+            )
