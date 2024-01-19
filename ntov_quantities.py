e1f62178f824ab7f307fd113bfe3709c9d10ea41
Eliot Finch <eliotfinch@gmail.com>
2024-01-18 16:17:43 -0800

    Add ntov_quantities.py

---
+++ ntov_quantities.py
@@ -0,0 +1,68 @@
+#!/home/eliot.finch/eos/env/bin/python
+
+import numpy as np
+import pandas as pd
+
+from scipy.interpolate import interp1d
+
+from utils import (
+    to_GeV_per_cubic_femtometre,
+    to_nucleons_per_cubic_femtometre,
+)
+
+collated_eos_path = '/home/isaac.legred/PTAnalysis/Analysis/collated_np_all_post.csv'
+eos_dir = '/home/philippe.landry/nseos/eos/gp/mrgagn'
+
+# Load the collated EOSs and filter out the ones with zero weight
+collated_eos = pd.read_csv(collated_eos_path)
+nonzero_collated_eos = collated_eos[collated_eos.logweight_total > -np.inf]
+
+# The central density for which the mass reaches a maximum Mmax
+ntov_array = to_nucleons_per_cubic_femtometre(nonzero_collated_eos['rhoc(M@Mmax)'])
+
+# Load the full EoS data from the GP draws, and interpolate onto a consistent
+# energy density grid
+
+energy_density_grid = np.linspace(1e-10, 5, 1000)
+pressure_interp = []
+number_density_interp = []
+
+for eos in nonzero_collated_eos.eos:
+
+    eos = int(eos)
+
+    df = pd.read_csv(f'{eos_dir}/DRAWmod1000-{int(eos/1000):06}/eos-draw-{eos:06}.csv')
+
+    pressure = to_GeV_per_cubic_femtometre(df.pressurec2).values
+    energy_density = to_GeV_per_cubic_femtometre(df.energy_densityc2).values
+    number_density = to_nucleons_per_cubic_femtometre(df.baryon_density).values
+
+    pressure_interp.append(interp1d(energy_density, pressure)(energy_density_grid))
+    number_density_interp.append(interp1d(energy_density, number_density)(energy_density_grid))
+
+pressure_interp = np.array(pressure_interp)
+number_density_interp = np.array(number_density_interp)
+
+# Get the energy density and pressure at ntov
+
+e_list = []
+p_list = []
+
+for p_grid, n_grid, ntov in zip(pressure_interp, number_density_interp, ntov_array):
+
+    # Requirement of the marginalized QCD likelihood
+    if ntov < 35*0.16:
+
+        index = np.argmin(np.abs(n_grid - ntov))
+        e = energy_density_grid[index]
+        p = p_grid[index]
+
+        e_list.append(e)
+        p_list.append(p)
+
+e_list = np.array(e_list)
+p_list = np.array(p_list)
+
+# Save to disk
+np.savetxt('ntov_quantities/energy_density.dat', e_list)
+np.savetxt('ntov_quantities/pressure.dat', p_list)
