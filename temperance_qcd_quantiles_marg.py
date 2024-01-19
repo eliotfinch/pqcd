23ba82ce7a0dc7bb7482879a5da36c4bf829e4c0
Eliot Finch <eliotfinch@gmail.com>
2024-01-19 15:43:48 -0800

    Add temperance_qcd_quantiles_marg.py

---
+++ temperance_qcd_quantiles_marg.py
@@ -0,0 +1,58 @@
+#!/home/eliot.finch/eos/env/bin/python
+
+import temperance.core.result as result
+import temperance.plotting.get_quantiles as get_quantiles
+
+from temperance.core.result import EoSPosterior
+
+# eos_posterior = EoSPosterior.from_csv("collated_np_all_post.csv", label="astro")
+eos_posterior = EoSPosterior.from_csv('collated_eos.csv', label='astro')
+
+astro_weight_columns = [
+    result.WeightColumn(name='weight_marg', is_log=False, is_inverted=False)
+    ]
+
+# Pressure vs energy density
+posterior_quantiles = get_quantiles.get_p_of_eps_quantiles(
+    eos_posterior, 
+    weight_columns=astro_weight_columns, 
+    verbose=True, 
+    max_num_samples=80000,
+    save_path=f'quantiles/p_of_eps_quantiles_marg.csv'
+    )
+
+# Pressure vs baryon density
+posterior_quantiles = get_quantiles.get_p_of_rho_quantiles(
+    eos_posterior, 
+    weight_columns=astro_weight_columns, 
+    verbose=True, 
+    max_num_samples=80000,
+    save_path=f'quantiles/p_of_rho_quantiles_marg.csv'
+    )
+
+# Speed of sound vs baryon density
+posterior_quantiles = get_quantiles.get_cs2_of_rho_quantiles(
+    eos_posterior, 
+    weight_columns=astro_weight_columns, 
+    verbose=True, 
+    max_num_samples=80000,
+    save_path=f'quantiles/cs2_of_rho_quantiles_marg.csv'
+    )
+
+# Mass vs radius
+posterior_quantiles = get_quantiles.get_r_of_m_quantiles(
+    eos_posterior, 
+    weight_columns=astro_weight_columns, 
+    verbose=True, 
+    max_num_samples=80000,
+    save_path=f'quantiles/r_of_m_quantiles_marg.csv'
+    )
+
+# Lambda vs mass
+posterior_quantiles = get_quantiles.get_lambda_of_m_quantiles(
+    eos_posterior, 
+    weight_columns=astro_weight_columns, 
+    verbose=True, 
+    max_num_samples=80000,
+    save_path=f'quantiles/lambda_of_m_quantiles_marg.csv'
+    )
