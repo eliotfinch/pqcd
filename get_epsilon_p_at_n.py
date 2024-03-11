93affa2ce7d1c9ebf404d81518f7761d433c1967
Eliot Finch <eliotfinch@gmail.com>
2024-03-11 12:19:04 -0700

    Update get_epsilon_p_at_n.py

--- get_epsilon_p_at_n.py
+++ get_epsilon_p_at_n.py
@@ -8,86 +8,60 @@ from scipy.interpolate import interp1d
 from utils import (
     to_GeV_per_cubic_femtometre,
     to_nucleons_per_cubic_femtometre,
+    nsat
 )
 
-collated_eos_path = '/home/isaac.legred/PTAnalysis/Analysis/collated_np_all_post.csv'
+collated_eos_path = [
+    '/home/isaac.legred/PTAnalysis/Analysis/collated_np_all_post.csv',
+    'collated_eos.csv'
+    ][1]
 eos_dir = '/home/philippe.landry/nseos/eos/gp/mrgagn'
 
-# Load the collated EOSs and filter out the ones with zero weight
+# Load the collated EOSs 
 collated_eos = pd.read_csv(collated_eos_path)
-nonzero_collated_eos = collated_eos[collated_eos.logweight_total > -np.inf]
 
 # The central density for which the mass reaches a maximum Mmax
-ntov_array = to_nucleons_per_cubic_femtometre(nonzero_collated_eos['rhoc(M@Mmax)'])
+collated_ntov = to_nucleons_per_cubic_femtometre(collated_eos['rhoc(M@Mmax)'])/nsat
 
-# Nuclear saturation density in fm^-3
-nsat = 0.16
+# The number densities at which to extract the energy density and pressure
+n0_list = [1,2,3,4,5,6,7,8,9,10,15,20]
 
-# Load the full EoS data from the GP draws, and interpolate onto a consistent
-# energy density grid
+# Dicts to store the pressures and energy densities at the requested densities
+p0_dict = {n0: [] for n0 in n0_list}
+e0_dict = {n0: [] for n0 in n0_list}
 
-energy_density_grid = np.linspace(1e-10, 5, 1000)
-pressure_interp = []
-number_density_interp = []
+# Lists to store pressure and energy density at ntov
+ptov_list = []
+etov_list = []
 
-for eos in nonzero_collated_eos.eos:
+# For each EOS, interpolate the pressure and energy density as a function of
+# number density and extract quantities at the requested densities
+for eos, ntov in zip(collated_eos.eos, collated_ntov):
 
-    eos = int(eos)
-
-    df = pd.read_csv(f'{eos_dir}/DRAWmod1000-{int(eos/1000):06}/eos-draw-{eos:06}.csv')
+    df = pd.read_csv(
+        f'{eos_dir}/DRAWmod1000-{int(eos/1000):06}/eos-draw-{int(eos):06}.csv'
+        )
 
     pressure = to_GeV_per_cubic_femtometre(df.pressurec2).values
     energy_density = to_GeV_per_cubic_femtometre(df.energy_densityc2).values
-    number_density = to_nucleons_per_cubic_femtometre(df.baryon_density).values
+    number_density = to_nucleons_per_cubic_femtometre(df.baryon_density).values/nsat
 
-    pressure_interp.append(interp1d(energy_density, pressure)(energy_density_grid))
-    number_density_interp.append(interp1d(energy_density, number_density)(energy_density_grid))
+    # Build an interpolant over number density
+    pressure_interp = interp1d(number_density, pressure, bounds_error=False)
+    energy_density_interp = interp1d(number_density, energy_density, bounds_error=False)
 
-pressure_interp = np.array(pressure_interp)
-number_density_interp = np.array(number_density_interp)
+    # Extract the energy density and pressure at the required densities
+    for n0 in n0_list:
+        p0_dict[n0].append(pressure_interp(n0))
+        e0_dict[n0].append(energy_density_interp(n0))
 
-# Get the energy density and pressure at ntov
-
-e_list = []
-p_list = []
-
-for p_grid, n_grid, ntov in zip(pressure_interp, number_density_interp, ntov_array):
-
-    index = np.argmin(np.abs(n_grid - ntov))
-    e = energy_density_grid[index]
-    p = p_grid[index]
-
-    e_list.append(e)
-    p_list.append(p)
-
-e_list = np.array(e_list)
-p_list = np.array(p_list)
+    ptov_list.append(pressure_interp(ntov))
+    etov_list.append(energy_density_interp(ntov))
 
 # Save to disk
-np.savetxt('ntov_quantities/energy_density.dat', e_list)
-np.savetxt('ntov_quantities/pressure.dat', p_list)
+for n0 in n0_list:
+    np.savetxt(f'quantities_at_fixed_n/pressure_{n0:02}nsat.dat', p0_dict[n0])
+    np.savetxt(f'quantities_at_fixed_n/energy_density_{n0:02}nsat.dat', e0_dict[n0])
 
-# Get the energy density and pressure at a fixed number density
-
-# n0_list = np.array([1,3,5,7,9,10])*nsat
-
-# for n0 in n0_list:
-
-#     e_list = []
-#     p_list = []
-
-#     for p_grid, n_grid in zip(pressure_interp, number_density_interp):
-
-#         index = np.argmin(np.abs(n_grid - n0))
-#         e = energy_density_grid[index]
-#         p = p_grid[index]
-
-#         e_list.append(e)
-#         p_list.append(p)
-
-#     e_list = np.array(e_list)
-#     p_list = np.array(p_list)
-
-#     # Save to disk
-#     np.savetxt(f'fixed_n_quantities/energy_density_{int(n0/nsat):02}nsat.dat', e_list)
-#     np.savetxt(f'fixed_n_quantities/pressure_{int(n0/nsat):02}nsat.dat', p_list)
+np.savetxt('quantities_at_ntov/pressure.dat', ptov_list)
+np.savetxt('quantities_at_ntov/energy_density.dat', etov_list)
