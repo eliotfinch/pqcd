e66d3e6d348ad77310968a80fd905760296d383a
Eliot Finch <eliotfinch@gmail.com>
2024-07-29 16:38:16 -0700

    Add RADIO astro inference

---
+++ astro_inference/RADIO/weigh_eos_set_by_pulsar.py
@@ -0,0 +1,30 @@
+import temperance as tmpy
+import temperance.core.result as result
+from  temperance.core.result import EoSPosterior
+import  temperance.weighing.weigh_by_pulsar  as weigh_by_pulsar
+
+import numpy as np
+import pandas as pd
+
+def pulsar_sample_likelihood(mmax_eos, mass_samples):
+    return np.array([np.sum(mass_samples < mmax)/len(mass_samples) for mmax in mmax_eos])
+
+
+def weigh_by_pulsar_data(pulsar_mass_sample_sets, eos_posterior, **kwargs):
+    for sample_set in pulsar_mass_sample_sets.keys():
+        weigh_by_pulsar.weigh_EoSs_by_mass_measurement(
+            eos_posterior, likelihood=lambda mmax : pulsar_sample_likelihood(mmax, pulsar_mass_sample_sets[sample_set]["m"]),
+            weight_tag = sample_set, **kwargs
+        )
+if __name__ == "__main__":
+    pulsar_mass_sets = {"Fonseca_J0740": pd.read_csv("/home/isaac.legred/New_NICER/NoNSAnalysis/CalcSamples/Fonseca_J0740.csv"),
+    "Antoniadis_J0348": pd.read_csv("/home/isaac.legred/New_NICER/NoNSAnalysis/CalcSamples/Antoniadis_J0348.csv")}
+    for eos_set in [
+            "11233_maxpc2-1e11_mrgagn_01d000_00d010",
+            "11233_maxpc2-1e12_mrgagn_01d000_00d010",
+            "11233_maxpc2-1e13_mrgagn_01d000_00d010",
+            "11233_maxpc2-1e14_mrgagn_01d000_00d010"
+    ]:
+        eos_posterior = EoSPosterior.from_csv(f"../{eos_set}.csv")
+        weigh_by_pulsar_data(pulsar_mass_sets, eos_posterior)
+        eos_posterior.samples.to_csv(f"../{eos_set}.csv", index=False)
