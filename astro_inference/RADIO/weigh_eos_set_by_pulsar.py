aeff3b31d502e2f54a5b3a3409a58a8ed2036e90
Eliot Finch <eliotfinch@gmail.com>
2024-07-29 16:47:10 -0700

    Update astro_inference scripts

--- astro_inference/RADIO/weigh_eos_set_by_pulsar.py
+++ astro_inference/RADIO/weigh_eos_set_by_pulsar.py
@@ -1,14 +1,11 @@
-import temperance as tmpy
-import temperance.core.result as result
-from  temperance.core.result import EoSPosterior
-import  temperance.weighing.weigh_by_pulsar  as weigh_by_pulsar
-
 import numpy as np
 import pandas as pd
 
+import temperance.weighing.weigh_by_pulsar as weigh_by_pulsar
+from temperance.core.result import EoSPosterior
+
 def pulsar_sample_likelihood(mmax_eos, mass_samples):
     return np.array([np.sum(mass_samples < mmax)/len(mass_samples) for mmax in mmax_eos])
-
 
 def weigh_by_pulsar_data(pulsar_mass_sample_sets, eos_posterior, **kwargs):
     for sample_set in pulsar_mass_sample_sets.keys():
@@ -17,14 +14,21 @@ def weigh_by_pulsar_data(pulsar_mass_sample_sets, eos_posterior, **kwargs):
             weight_tag = sample_set, **kwargs
         )
 if __name__ == "__main__":
-    pulsar_mass_sets = {"Fonseca_J0740": pd.read_csv("/home/isaac.legred/New_NICER/NoNSAnalysis/CalcSamples/Fonseca_J0740.csv"),
-    "Antoniadis_J0348": pd.read_csv("/home/isaac.legred/New_NICER/NoNSAnalysis/CalcSamples/Antoniadis_J0348.csv")}
-    for eos_set in [
-            "11233_maxpc2-1e11_mrgagn_01d000_00d010",
-            "11233_maxpc2-1e12_mrgagn_01d000_00d010",
-            "11233_maxpc2-1e13_mrgagn_01d000_00d010",
-            "11233_maxpc2-1e14_mrgagn_01d000_00d010"
-    ]:
-        eos_posterior = EoSPosterior.from_csv(f"../{eos_set}.csv")
+
+    set_number = 12
+    varieties = ['had', 'hyp', 'qrk']
+    
+    eos_base_directory = f'/home/eliot.finch/eos/pqcd/data/eos-draws-modified/{set_number}'
+
+    pulsar_mass_sets = {
+        "Fonseca_J0740": pd.read_csv("/home/isaac.legred/New_NICER/NoNSAnalysis/CalcSamples/Fonseca_J0740.csv"),
+        "Antoniadis_J0348": pd.read_csv("/home/isaac.legred/New_NICER/NoNSAnalysis/CalcSamples/Antoniadis_J0348.csv")
+        }
+    
+    for variety in varieties:
+        eos_posterior = EoSPosterior.from_csv(
+            f"{eos_base_directory}/{variety}agn.csv", 
+            label=f'{variety}agn'
+            )
         weigh_by_pulsar_data(pulsar_mass_sets, eos_posterior)
-        eos_posterior.samples.to_csv(f"../{eos_set}.csv", index=False)
+        eos_posterior.samples.to_csv(f"{variety}agn.csv", index=False)
--- astro_inference/get_eos_files.py
+++ astro_inference/get_eos_files.py
@@ -1,27 +1,28 @@
-"""
-Get a file to store EoS properties for each EoS set, including likelihoods. 
-"""
-import temperance as tmpy
+#!/home/eliot.finch/eos/env/bin/python
+
+import numpy as np
+import pandas as pd
 import temperance.sampling.eos_prior as tmeos_prior
 
-import pandas as pd
-import numpy as np
 import tqdm
-import os
 
 def get_mmax(macro_data):
     dM_drho = np.gradient(macro_data["M"], macro_data["central_baryon_density"])
     stable = np.where(dM_drho > 0)[0]
     return np.max(macro_data["M"][stable])
     
-
-
 def get_eos_file(eos_directory, properties = {"Mmax": get_mmax}, num_eoss=10, eos_per_dir=1000):
     """
     For each eos, get all of the properties specified in the properties (currently only can get
     properties from the "macro" file)
     """
-    eos_prior_set = tmeos_prior.EoSPriorSet(eos_dir = eos_directory,eos_column= "eos", eos_per_dir=eos_per_dir, macro_dir=eos_directory, branches_data=None)
+    eos_prior_set = tmeos_prior.EoSPriorSet(
+        eos_dir = eos_directory,
+        eos_column= "eos", 
+        eos_per_dir=eos_per_dir, 
+        macro_dir=eos_directory, 
+        branches_data=None
+        )
     eos_file = pd.DataFrame({"eos" : np.arange(num_eoss) })
     for prop_name in properties.keys():
         eos_file[prop_name] = np.zeros(num_eoss)
@@ -32,12 +33,16 @@ def get_eos_file(eos_directory, properties = {"Mmax": get_mmax}, num_eoss=10, eo
     return eos_file
 
 if __name__ == "__main__":
-    for directory in [
-        "hadagn",
-        "hypagn",
-        "qrkagn"
-    ]:
+
+    set_number = 12
+    count_dict = {'had': 84, 'hyp': 338, 'qrk': 437}
+    
+    for variety, count in count_dict.items():
         eos_file = get_eos_file(
-            os.path.join("/home/eliot.finch/eos/pqcd/make-agnostic-processes", directory))
-        eos_file.to_csv(f"{directory}.csv", index=False)
-            
+            f'/home/eliot.finch/eos/pqcd/data/eos-draws-modified/{set_number}/{variety}agn', 
+            num_eoss=count
+            )
+        eos_file.to_csv(
+            f'/home/eliot.finch/eos/pqcd/data/eos-draws-modified/{set_number}/{variety}agn.csv', 
+            index=False
+            )
