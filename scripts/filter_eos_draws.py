385e42a4ad908039714989e80c8c31f3fa97e1a5
Eliot Finch <eliotfinch@gmail.com>
2024-08-15 12:01:39 -0700

    Automatically construct success_counts dictionary

--- scripts/filter_eos_draws.py
+++ scripts/filter_eos_draws.py
@@ -6,23 +6,56 @@ import shutil
 import os
 import pqcd
 
-source_dir = '/home/eliot.finch/eos/pqcd/make-agnostic-processes'
-destination_dir = '/home/eliot.finch/eos/pqcd/data/eos-draws-modified/12'
-success_counts = {'hyp': 339, 'qrk': 438}
+from pathlib import Path
 
-N_samp = 50000
+source_dir = Path('/home/eliot.finch/eos/pqcd/make-agnostic-processes')
+destination_dir = Path('/home/eliot.finch/eos/pqcd/data/eos-draws-modified/12')
 
-pqcd_region_dict = pqcd.get_pqcd_region(mu_high=3, res=200)
+varieties = ['had', 'hyp', 'qrk']
+success_counts = {}
 
-for variety in ['hyp', 'qrk']:
-    success_count = success_counts[variety]
-    for n in range(N_samp):
-        source_path = f'{source_dir}/{variety}agn/DRAWmod1000-{int(n/1000):06}'
-        eos = pd.read_csv(f'{source_path}/eos-draw-{n:06}.csv')
-        if pqcd.consistent_with_pqcd(eos, pqcd_region_dict):
-            destination_path = f'{destination_dir}/{variety}agn/DRAWmod1000-{int(success_count/1000):06}'
-            if not os.path.exists(destination_path):
-                os.makedirs(destination_path)
-            shutil.copy(f'{source_path}/eos-draw-{n:06}.csv', f'{destination_path}/eos-draw-{success_count:06}.csv')
-            shutil.copy(f'{source_path}/draw-gpr_{variety}agn-{n:06}.csv', f'{destination_path}/draw-gpr_{variety}agn-{success_count:06}.csv')
-            success_count += 1
+for variety in varieties:
+
+    var_dest_dir = destination_dir / f'{variety}agn'
+
+    # Get the list of DRAWmod1000 directories
+    drawmod_dirs = [
+        f.name for f in var_dest_dir.iterdir() 
+        if f.is_dir() and f.name.startswith('DRAWmod1000')
+        ]
+
+    # We care about the largest DRAWmod1000 directory
+    drawmod_dir = sorted(drawmod_dirs)[-1]
+
+    drawmod_dest_dir = var_dest_dir / drawmod_dir
+
+    # Get the list of eos-draw files
+    eos_draw_files = [
+        f.name for f in drawmod_dest_dir.iterdir() 
+        if f.is_file() and f.name.startswith('eos-draw')
+        ]
+    
+    eos_numbers = []
+    for f in eos_draw_files:
+        eos_numbers.append(int(f.split('-')[-1].split('.')[0]))
+
+    success_counts[variety] = max(eos_numbers) + 1
+
+print(success_counts)
+
+# N_samp = 50000
+
+# pqcd_region_dict = pqcd.get_pqcd_region(mu_high=3, res=200)
+
+# for variety in varieties:
+#     success_count = success_counts[variety]
+#     for n in range(N_samp):
+#         source_path = source_dir / f'{variety}agn/DRAWmod1000-{int(n/1000):06}'
+#         eos = pd.read_csv(source_path / f'eos-draw-{n:06}.csv')
+#         if pqcd.consistent_with_pqcd(eos, pqcd_region_dict):
+#             destination_path = destination_dir / f'{variety}agn/DRAWmod1000-{int(success_count/1000):06}'
+#             if not os.path.exists(destination_path):
+#                 os.makedirs(destination_path)
+#             shutil.copy(source_path / f'eos-draw-{n:06}.csv', destination_path / f'eos-draw-{success_count:06}.csv')
+#             shutil.copy(source_path / f'draw-gpr_{variety}agn-{n:06}.csv', destination_path / f'draw-gpr_{variety}agn-{success_count:06}.csv')
+#             success_count += 1
