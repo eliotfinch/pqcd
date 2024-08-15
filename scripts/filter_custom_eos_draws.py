8f5e66df985443fed1c525f51fb08dc5f0218cb7
Eliot Finch <eliotfinch@gmail.com>
2024-08-15 10:17:44 -0700

    Add filter_custom_eos_draws.py

---
+++ scripts/filter_custom_eos_draws.py
@@ -0,0 +1,28 @@
+#!/home/eliot.finch/eos/env/bin/python
+
+import pandas as pd
+
+import shutil
+import os
+import pqcd
+
+source_dir = '/home/eliot.finch/eos/pqcd/make-agnostic-processes'
+destination_dir = '/home/eliot.finch/eos/pqcd/data/eos-draws-modified/12'
+success_counts = {'hyp': 339, 'qrk': 438}
+
+N_samp = 50000
+
+pqcd_region_dict = pqcd.get_pqcd_region(mu_high=3, res=200)
+
+for variety in ['hyp', 'qrk']:
+    success_count = success_counts[variety]
+    for n in range(N_samp):
+        source_path = f'{source_dir}/{variety}agn/DRAWmod1000-{int(n/1000):06}'
+        eos = pd.read_csv(f'{source_path}/eos-draw-{n:06}.csv')
+        if pqcd.consistent_with_pqcd(eos, pqcd_region_dict):
+            destination_path = f'{destination_dir}/{variety}agn/DRAWmod1000-{int(success_count/1000):06}'
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+            shutil.copy(f'{source_path}/eos-draw-{n:06}.csv', f'{destination_path}/eos-draw-{success_count:06}.csv')
+            shutil.copy(f'{source_path}/draw-gpr_{variety}agn-{n:06}.csv', f'{destination_path}/draw-gpr_{variety}agn-{success_count:06}.csv')
+            success_count += 1
