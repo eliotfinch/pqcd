2ddc813d8cf58714e4c7f3ab2f9f729ccf052712
Eliot Finch <eliotfinch@gmail.com>
2024-11-22 11:30:45 -0800

    Fix collated eos  path

--- scripts/pqcd_quantiles.py
+++ scripts/pqcd_quantiles.py
@@ -13,7 +13,7 @@ default_eos_prior.macro_dir = '/home/philippe.landry/nseos/eos/gp/mrgagn/'
 
 # All draws
 eos_posterior = EoSPosterior.from_csv(
-    '../data/eos-draws-default/collated_np_all_post_with_prior.csv',
+    '../data/eos-draws-default/collated_np_all_post_edit.csv',
 )
 
 # Marginalised over X
