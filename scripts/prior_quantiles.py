697744e03ab0f9aa7a87600df35d8e98ec901694
Eliot Finch <eliotfinch@gmail.com>
2024-12-09 20:07:11 -0800

    Prepare mass vs radius prior quantiles

--- scripts/prior_quantiles.py
+++ scripts/prior_quantiles.py
@@ -42,27 +42,29 @@ weight_columns = [
 #     save_path='../data/quantiles/p_of_rho_quantiles.csv'
 #     )
 
-# Speed of sound vs baryon density
-posterior_quantiles = get_quantiles.get_cs2_of_rho_quantiles(
+# # Speed of sound vs baryon density
+# posterior_quantiles = get_quantiles.get_cs2_of_rho_quantiles(
+#     eos_posterior,
+#     eos_data=default_eos_prior,
+#     weight_columns=weight_columns,
+#     verbose=True,
+#     max_num_samples=160000,
+#     x_points=np.linspace(2.8e13, 2.8e15, 1000),
+#     save_path=(
+#         '../data/eos-draws-default/quantiles/cs2_of_rho_quantiles_prior.csv'
+#     )
+# )
+
+# Mass vs radius
+posterior_quantiles = get_quantiles.get_r_of_m_quantiles(
     eos_posterior,
     eos_data=default_eos_prior,
     weight_columns=weight_columns,
     verbose=True,
     max_num_samples=160000,
-    x_points=np.linspace(2.8e13, 2.8e15, 1000),
-    save_path=(
-        '../data/eos-draws-default/quantiles/cs2_of_rho_quantiles_prior.csv'
+    x_points=np.linspace(0.5, 2.5, 1000),
+    save_path='../data/eos-draws-default/r_of_m_quantiles_prior.csv'
     )
-)
-
-# # Mass vs radius
-# posterior_quantiles = get_quantiles.get_r_of_m_quantiles(
-#     eos_posterior,
-#     weight_columns=astro_weight_columns,
-#     verbose=True,
-#     max_num_samples=80000,
-#     save_path='../data/quantiles/r_of_m_quantiles.csv'
-#     )
 
 # # Lambda vs mass
 # posterior_quantiles = get_quantiles.get_lambda_of_m_quantiles(
