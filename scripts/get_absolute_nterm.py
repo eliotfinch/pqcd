d5785b5ff7a81fe9826ab3a296e9ab979b9c5694
Eliot Finch <eliotfinch@gmail.com>
2024-12-11 14:07:25 -0800

    Update get_absolute_nterm.py

--- scripts/get_absolute_nterm.py
+++ scripts/get_absolute_nterm.py
@@ -9,26 +9,35 @@ from pqcd.utils import (
     eos_dir
 )
 
-collated_eos_path = '../data/eos-draws-default.csv'
-
-# Load the collated EOSs and filter out the ones with zero weight
+collated_eos_path = '../data/eos-draws-default/collated_np_all_post.csv'
 collated_eos = pd.read_csv(collated_eos_path)
 
+pterm = []
+epsilon_term = []
 nterm = []
 
 for eos in collated_eos.eos:
 
     eos = int(eos)
 
-    df = pd.read_csv(f'{eos_dir}/DRAWmod1000-{int(eos/1000):06}/eos-draw-{eos:06}.csv')
+    df = pd.read_csv(
+        f'{eos_dir}/DRAWmod1000-{int(eos/1000):06}/eos-draw-{eos:06}.csv'
+    )
 
     pressure = to_GeV_per_cubic_femtometre(df.pressurec2).values
     energy_density = to_GeV_per_cubic_femtometre(df.energy_densityc2).values
     number_density = to_nucleons_per_cubic_femtometre(df.baryon_density).values
 
-    nterm.append(number_density[-1])
+    if len(pressure) == len(energy_density) == len(number_density):
+        pterm.append(pressure[-1])
+        epsilon_term.append(energy_density[-1])
+        nterm.append(number_density[-1])
+    else:
+        print(f'eos {eos} not equal length!')
 
-nterm = np.array(nterm)
-
-# Save the nterm to disk
-np.savetxt(f'../data/eos-draws-default-nterm.dat', nterm)
+np.savetxt('../data/eos-draws-default/eos-draws-default-pterm.dat', pterm)
+np.savetxt(
+    '../data/eos-draws-default/eos-draws-default-epsilon_term.dat',
+    epsilon_term
+)
+np.savetxt('../data/eos-draws-default/eos-draws-default-nterm.dat', nterm)
